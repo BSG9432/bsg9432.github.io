07d50a28b4a27c87f5b36a74bf55d897ba411b5b
Jatnael Montes <293746@my.puhsd.org>
2019-04-23 22:32:03 -0700

    Time to rebuild a website

--- rover-ruckus/YesNoButton.js
+++
@@ -1,8 +0,0 @@
-import ToggleButton from '../ToggleButton'
-
-export default () => (
-  <ToggleButton
-    values={['yes', 'no']}
-    onChange={e => this.setState({ robot2Landing: 1 - e.selected })}
-  />
-)
